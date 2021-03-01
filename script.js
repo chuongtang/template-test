6898d8794e7f18d7a466cca8b065a0bfcae63b4b
chuongtang <chuongtang@users.noreply.github.com>
2021-03-01 09:55:11 -0700

    using Git to transfer to new PC

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get quote from API //
+async function getQuote() {
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+
+    } catch (error) {
+        console.log('whooopsss, no quote', error);
+    }
+}
+// On Load //
+getQuote();
